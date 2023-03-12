156ffded7ab0ff845482754093a2c396fec4c9e5
dibavosta <diba.vosta@gmail.com>
2023-03-12 10:56:58 +0100

    Form for count down

--- components/CountDownElement.tsx
+++ components/CountDownElement.tsx
@@ -2,7 +2,7 @@ import { TimeType } from "@/enums/TimeType";
 import { Locale } from "@/types/Locale";
 import { t } from "i18next";
 import { useTranslation } from "next-i18next";
-import formStyles from "@/components/form.module.css";
+import formStyles from "@/components/Form.module.css";
 import styles from "@/components/CountDown.module.css";
 
 interface CountDownElementProps {
